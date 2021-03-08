9019925f7bce06e95c918082b9c6d108f6980bde
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-03-08 22:34:12 +0600

    "#2 Исправлен функционал по выведению списка продуктов в модалке"

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -2,12 +2,14 @@ import { ListItemText } from "@material-ui/core";
 
 export default function OrderSummary({ ingredients, total, delivery, price }) {
   const ingredientsSummary = Object.keys(ingredients).map((ingKey) => {
-    return (
-      <ListItemText key={ingKey}>
-        <span style={{ textTransform: "capitalize" }}>{ingKey}</span>:{" "}
-        {ingredients[ingKey]} <span>x {price[ingKey]} KZT</span>
-      </ListItemText>
-    );
+    if (ingredients[ingKey] !== 0) {
+      return (
+        <ListItemText key={ingKey}>
+          <span style={{ textTransform: "capitalize" }}>{ingKey}</span>:{" "}
+          {ingredients[ingKey]} <span>x {price[ingKey]} KZT</span>
+        </ListItemText>
+      );
+    } else return null;
   });
   return (
     <>
